42bb01dfd4dfb5804fac6b8df881114ec26935f8
Luka Stanisic <luka.stanisic@mpcdf.mpg.de>
2018-01-11 14:59:22 +0100

    doc: changing style from Alabaster to Read the Docs Sphinx Theme

--- doc/conf.py
+++ doc/conf.py
@@ -90,13 +90,37 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+# Change from Alabaster to RTD theme, check
+# https://sphinx-rtd-theme.readthedocs.io/en/latest/ for more details
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
+
+# Change from Alabaster to RTD theme, check
+# https://sphinx-rtd-theme.readthedocs.io/en/latest/ for more details
 # html_theme_options = {}
+html_theme_options = {
+    'typekit_id': '',
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+#    'prev_next_buttons_location': bottom,
+    'prev_next_buttons_location': None,
+    'style_external_links': False,
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': False,
+    'navigation_depth': 3,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -108,12 +132,15 @@ html_static_path = ['_static']
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
+
+# Change from Alabaster to RTD theme, check
+# https://sphinx-rtd-theme.readthedocs.io/en/latest/ for more details
+# html_sidebars = {
+#     '**': [
+#         'relations.html',  # needs 'show_related': True theme option to display
+#         'searchbox.html',
+#     ]
+# }
 
 
 # -- Options for HTMLHelp output ------------------------------------------
